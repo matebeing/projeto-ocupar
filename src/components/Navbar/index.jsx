cab7164c7b1a7628cbbe70386c87d749792b7a83
Matheus Oliveira <matheusoliveira18rj@gmail.com>
2024-09-26 16:41:05 -0300

    fix: my last commit

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,11 +1,11 @@
 import Logo from '../../assets/logo.png'
 const Navbar = () => {
     return (
-        <nav className="flex items-center justify-between sticky border-b-2 py-4">
+        <nav className="flex items-center justify-between sticky border-b-2 py-4 ">
             <section className='flex items-center justify-between w-full lg:max-w-[1100px] lg:mx-auto'>
                 <section className='flex flex-column'>
                     <img src={Logo} alt="logo" className='w-20' />
-                    <ul className='flex items-center ml-20 gap-12 flex-row text-lg font-medium md:hidden '>
+                    <ul className='flex items-center ml-20 gap-12 flex-row text-lg font-medium'>
                         <li>Inicio</li>
                         <li>Projetos</li>
                         <li>Quem somos</li>
