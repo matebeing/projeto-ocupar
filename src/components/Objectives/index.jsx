d7f83edc5ae56bc37bfb4ef669dcbb090ef42c22
Matheus Oliveira <matheusoliveira18rj@gmail.com>
2024-10-15 11:47:38 -0300

    feat: numbers on goals

--- src/components/Objectives/index.jsx
+++ src/components/Objectives/index.jsx
@@ -7,11 +7,23 @@ const Objectives = () => {
                 <p className='text-2xl text-center text-[#909090] w-[60rem] lg:max-w-[1100px] sm:w-full sm:px-5'>O Ocupar é um projeto construído por estudantes universitários com o propósito de democratizar o acesso à informação para que os estudantes de escola pública ocupem as universidades. </p>
             </section>
             <section>
-                <ul className='flex lg:flex-row sm:flex-col gap-10 text-lg text-[#F09048] font-extrabold'>
-                    <li className='flex items-center justify-center border-[10px] p-20 border-[#F09048] w-40 h-40 rounded-full'>OCUPAR</li>
-                    <li className='flex items-center justify-center border-[10px] p-20 border-[#F09048] w-40 h-40 rounded-full'>INFORMAR</li>
-                    <li className='flex items-center justify-center border-[10px] p-20 border-[#F09048] w-40 h-40 rounded-full'>DIVERSIFICAR</li>
-                    <li className='flex items-center justify-center border-[10px] p-20 border-[#F09048] w-40 h-40 rounded-full'>DEMOCRATIZAR</li>
+                <ul className='flex lg:flex-row sm:flex-col gap-10 text-1xl text-[#F09048] font-extrabold'>
+                    <li className="flex items-center justify-center border-8 border-[#F09048] rounded-full w-40 h-40 relative">
+                        <span>OCUPAR</span>
+                        <div className="flex items-center justify-center text-center text-white text-2xl font-bold absolute rounded-full left-[-10px] top-[-10px] h-14 w-14 bg-[#F09048]">1</div>
+                    </li>
+                    <li className="flex items-center justify-center border-8 border-[#F09048] rounded-full w-40 h-40 relative">
+                        <span>INFORMAR</span>
+                        <div className="flex items-center justify-center text-white text-2xl font-bold absolute rounded-full left-[-10px] top-[-10px] h-14 w-14 bg-[#F09048]">2</div>
+                    </li>
+                    <li className="flex items-center justify-center border-8 border-[#F09048] rounded-full w-40 h-40 relative">
+                        <span>DIVERSIFICAR</span>
+                        <div className="flex items-center justify-center text-white text-2xl font-bold absolute rounded-full left-[-10px] top-[-10px] h-14 w-14 bg-[#F09048]">3</div>
+                    </li>
+                    <li className="flex items-center justify-center border-8 border-[#F09048] rounded-full w-40 h-40 relative">
+                        <span>DEMOCRATIZAR</span>
+                        <div className="flex items-center justify-center text-white text-2xl font-bold absolute rounded-full left-[-10px] top-[-10px] h-14 w-14 bg-[#F09048]">4</div>
+                    </li>
                 </ul>
             </section>
             
